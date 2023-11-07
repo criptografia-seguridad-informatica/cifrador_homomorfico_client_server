3a624bbba983ca1791111d2d06d0b7d13949c0a2
amveiga <angelmartinveiga@gmail.com>
2023-11-07 10:47:06 -0300

    unifico los Dockerfiles

--- start_client.sh
+++ start_client.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Before running run in terminal sudo chmod +x start_client.sh
-docker build -t client:latest -f Dockerfile.client .
+docker build -t homomorphic_encryption_client:latest -f Dockerfile .
 docker rm homomorphic_encryption_client
-docker run -it --name homomorphic_encryption_client --network=host client:latest
+docker run -it --name homomorphic_encryption_client --network=host homomorphic_encryption_client:latest ./client/main.py
--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Before running run in terminal sudo chmod +x start_server.sh
-docker build -t server:latest -f Dockerfile.server .
+docker build -t homomorphic_encryption_server:latest -f Dockerfile .
 docker rm homomorphic_encryption_server
-docker run -it --name homomorphic_encryption_server -p 5000:5000 server:latest
+docker run -it --name homomorphic_encryption_server -p 5000:5000 homomorphic_encryption_server:latest ./server/main.py
