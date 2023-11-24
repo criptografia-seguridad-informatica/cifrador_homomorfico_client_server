7be19f5c7254b6dec978b77ecf6f6baa17988441
amveiga <angelmartinveiga@gmail.com>
2023-11-24 12:00:08 -0300

    feat: cifrador homomorfico completo realiza las mismas operaciones que uno parcial

--- test/cifrador_homomorfico_completo_test.py
+++ test/cifrador_homomorfico_completo_test.py
@@ -12,3 +12,42 @@ def test_cifrador_homomorfico_completo_desencripta_un_mensaje():
     numero_encriptado = cifrador_homomorfico_completo.encriptar(numero_a_encriptar)
     numero_desencriptado = cifrador_homomorfico_completo.desencriptar(numero_encriptado)
     assert numero_a_encriptar == numero_desencriptado
+
+def test_suma_de_un_numero_encriptado_y_uno_no_encriptado():
+    numero_a_encriptar = 10
+    numero_no_encriptado = 5
+
+    cifrador_homomorfico_completo = CifradorHomomorficoCompleto()
+
+    numero_encriptado = cifrador_homomorfico_completo.encriptar(numero_a_encriptar)
+
+    suma_encriptada = numero_encriptado + numero_no_encriptado
+    suma_desencriptada = cifrador_homomorfico_completo.desencriptar(suma_encriptada)
+
+    assert suma_desencriptada == 15
+
+def test_suma_de_dos_numeros_encriptados():
+    numero_a_encriptar_1 = 10
+    numero_a_encriptar_2 = 5
+
+    cifrador_homomorfico_completo = CifradorHomomorficoCompleto()
+
+    numero_encriptado_1 = cifrador_homomorfico_completo.encriptar(numero_a_encriptar_1)
+    numero_encriptado_2 = cifrador_homomorfico_completo.encriptar(numero_a_encriptar_2)
+
+    suma_encriptada = numero_encriptado_1 + numero_encriptado_2
+    suma_desencriptada = cifrador_homomorfico_completo.desencriptar(suma_encriptada)
+
+    assert suma_desencriptada == 15
+
+
+def test_multiplicacion_de_un_numero_encriptado_por_numero_no_encriptado():
+    numero_a_encriptar_1 = 5
+    numero_2 = 5
+
+    cifrador_homomorfico_completo = CifradorHomomorficoCompleto()
+    numero_encriptado_1 = cifrador_homomorfico_completo.encriptar(numero_a_encriptar_1)
+    multiplicacion_encriptada = numero_encriptado_1 * numero_2
+    multiplicacion_desencriptada = cifrador_homomorfico_completo.desencriptar(multiplicacion_encriptada)
+
+    assert multiplicacion_desencriptada == 25
